c49a8bb8890a4c9a33833c8a9ac65ad3f630b937
dimajolkin <dimajolkin@rambler.ru>
2016-06-21 17:54:25 +0300

    create work version

--- wsServer.py
+++ wsServer.py
@@ -1,3 +1,5 @@
+import json
+
 import tornado.httpserver
 import tornado.websocket
 import tornado.ioloop
@@ -8,59 +10,97 @@ import threading
 
 
 class RedisListener(threading.Thread):
-    def __init__(self, r, handler, channels):
+    handlers = []
+    redis = None
+
+    def attach_handler(self, handler):
+        self.handlers.append(handler)
+
+    def detach_handler(self, handler):
+        self.handlers.remove(handler)
+
+    def send_by_user(self, user_id, message):
+        for handler in self.handlers:
+            if isinstance(handler, WebSocketHandler):
+                if handler.user_profile_id == user_id:
+                    handler.write_message({'id': json.loads(message)})
+
+    def get(self, key):
+        return self.redis.get(key)
+
+    def set(self, key, value):
+        self.redis.set(key, value)
+
+    def __init__(self, redis, channels):
         """
 
+        :type redis: redis.client.Redis
         :type handler: WebSocketHandler
         """
+        self.redis = redis
         threading.Thread.__init__(self)
-        self.redis = r
-        self.handler = handler
-        self.pubsub = self.redis.pubsub()
-        self.pubsub.subscribe(channels)
+
+        self.pubsub = redis.pubsub()
+        self.pubsub.psubscribe(channels)
 
     def work(self, item):
-        print item['channel'], ":", item['data']
+        print(item['channel'], ":", item['data'])
 
     def close(self):
         self.pubsub.unsubscribe()
 
     def run(self):
         for item in self.pubsub.listen():
-            self.handler.write_message(item['data'])
             self.work(item)
+            try:
+                user_id = item['channel'].split(':')[1]
+                self.send_by_user(user_id, item['data'])
+
+            except Exception:
+                pass
 
 
 class WebSocketHandler(tornado.websocket.WebSocketHandler):
-    app = None
+    def __init__(self, application, request, **kwargs):
+        super(WebSocketHandler, self).__init__(application, request, **kwargs)
+
+    user_profile_id = None
+    token = None
 
     def open(self):
-        self.app = RedisListener(redis.Redis(), self, ["564"])
-        self.app.start()
-        print "new connection"
+        storage.attach_handler(self)
+        print("new connection {0}".format(len(storage.handlers)))
 
     def on_message(self, message):
-        self.write_message(message)
+        data = json.loads(message)
+        if 'token' in data:
+            self.token = data['token']
+            self.user_profile_id = storage.get('AUTH:{0}'.format(self.token))
+            if self.user_profile_id:
+                storage.set('STATUS:{0}'.format(self.user_profile_id), 1)
+
+            else:
+                self.write_message({"type": "error", "code": 401, "content": "User not authorization!"})
+                return
 
     def on_close(self):
-        if self.app is not None:
-            self.app.close()
-
-        print 'connection closed'
+        storage.detach_handler(self)
+        print('connection closed')
 
     def check_origin(self, origin):
         return True
 
 
 application = tornado.web.Application([
-    (r'/ws', WebSocketHandler),
+    (r'/pythonDaemon', WebSocketHandler),
 ])
 
-if __name__ == "__main__":
-    r = redis.Redis()
+storage = RedisListener(redis.Redis(db=2), ["NOTIF:*"])
+storage.start()
 
+if __name__ == "__main__":
     http_server = tornado.httpserver.HTTPServer(application)
     http_server.listen(8888)
     myIP = socket.gethostbyname(socket.gethostname())
-    print  '*** Websocket Server Started at %s***' % myIP
+    print('*** Websocket Server Started at %s***' % myIP)
     tornado.ioloop.IOLoop.instance().start()
